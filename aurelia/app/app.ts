0ebf8d1361a49534a40404acbf14375da32185a7
fshchudlo <fshchudlo@gmail.com>
2016-01-30 11:54:05 +0600

    route fixed

--- aurelia/app/app.ts
+++ aurelia/app/app.ts
@@ -4,8 +4,8 @@ export class App {
     configureRouter(config, router) {
         config.title = 'Welcome to Aurelia!';
         config.map([
-            { route: ['', 'component-sample'], name: 'ComponentSample', moduleId: 'app/component-sample/component-sample', nav: true, title: 'Component sample' },
-            { route: 'component-sample', name: 'BindingSample', moduleId: 'app/binding-sample/binding-sample', nav: true, title: 'Binding sample' }
+            { route: ['', 'component-sample'], moduleId: 'app/component-sample/component-sample', nav: true, title: 'Component sample' },
+            { route: 'binding-sample', moduleId: 'app/binding-sample/binding-sample', nav: true, title: 'Binding sample' }
         ]);
         this.router = router;
     }
