d8146ddfecb5d883aad3db0dccc8a5800f9d0dd9
fshchudlo <fshchudlo@gmail.com>
2016-03-25 21:35:55 +0600

    ng2 updated

--- angular2/app/app.ts
+++ angular2/app/app.ts
@@ -1,13 +1,14 @@
 import {Component} from 'angular2/core';
-import {View} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {ComponentSample} from './component-sample/component-sample';
+import {BindingSample} from './binding-sample/binding-sample';
 
 @Component({
     selector: 'angular-app', templateUrl: 'app/app.html', directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-    { path: '/component-sample', name: 'ComponentSample', loader : () => System.import('app/component-sample/component-sample').then(m => m.ComponentSample), useAsDefault: true },
-    { path: '/binding-sample', name: 'BindingSample', loader : () => System.import('app/binding-sample/binding-sample').then(m => m.BindingSample) }
+    { path: '/component-sample', name: 'ComponentSample', component: ComponentSample, useAsDefault: true },
+    { path: '/binding-sample', name: 'BindingSample', component: BindingSample }
 ])
 export class App {
     message: string = "Welcome to Angular2!";
